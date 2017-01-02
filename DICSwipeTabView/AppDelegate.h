292c777093c0421264e8c5a2d30b911d60228f8c
GAOCE <xdgcc1994@gmail.com>
2017-01-02 12:15:45 +0800

    Initial Commit

---
+++ DICSwipeTabView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DICSwipeTabView
+//
+//  Created by GAOCE on 2017/1/2.
+//  Copyright © 2017年 GAOCE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
