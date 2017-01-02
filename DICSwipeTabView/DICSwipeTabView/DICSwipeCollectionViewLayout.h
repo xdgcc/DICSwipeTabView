5cce30832b009fd812d15a534192d767055643ed
GAOCE <xdgcc@GAOCEdeMacBook-Pro.local>
2017-01-02 18:20:51 +0800

    给collectionview添加暂时的flowlayout。

--- DICSwipeTabView/DICSwipeTabView/DICSwipeCollectionViewLayout.h
+++ DICSwipeTabView/DICSwipeTabView/DICSwipeCollectionViewLayout.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DICSwipeCollectionViewLayout : UICollectionViewLayout
+@interface DICSwipeCollectionViewLayout : UICollectionViewFlowLayout
 
 @end
--- DICSwipeTabView/DICSwipeTabView/DICSwipeCollectionViewLayout.m
+++ DICSwipeTabView/DICSwipeTabView/DICSwipeCollectionViewLayout.m
@@ -8,6 +8,30 @@
 
 #import "DICSwipeCollectionViewLayout.h"
 
+static const CGFloat ItemHW = 50;
+
 @implementation DICSwipeCollectionViewLayout
+
+- (instancetype)init {
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+
+- (void)prepareLayout {
+    [super prepareLayout];
+    
+    //初始化每个 cell 的尺寸
+    self.itemSize = CGSizeMake(ItemHW*3, ItemHW);
+    self.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    //设置水平滚动
+    self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    self.minimumLineSpacing = 0;//ItemHW * 0.3;
+}
+
+
+
 
 @end
